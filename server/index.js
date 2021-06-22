cb92aec5e9326db5aa1a507cd333211c470e4df3
Max Ooi Wei Xiang <wxo_rocket0123@hotmail.com>
2021-06-23 01:22:45 +0800

    google oauth setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+const keys = require('./config/keys.js');
+
+passport.use(new GoogleStrategy({
+	clientID: keys.googleClientID,
+	clientSecret: keys.googleClientSecret,
+	callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log('Access token:', accessToken);
+	console.log('Refresh token:', refreshToken);
+	console.log('Profile:', profile);
+}
+));
 
 app.get('/',(req,res) =>{
 	res.send({ hi: 'there'});
 });
 
-const PORT = process.env.port || 5000
+app.get('/auth/google',
+	passport.authenticate('google',{
+		scope: ['profile','email']
+	})
+);
+
+app.get('/auth/google/callback',
+	passport.authenticate('google')
+);
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
